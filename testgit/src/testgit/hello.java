17e0a190e4da9858788dda10b2d6bf2cf4eba4be
songdaisong <36954321+songdaisong@users.noreply.github.com>
2018-09-04 16:38:03 +0800

    Update hello.java
    网上修改的

--- testgit/src/testgit/hello.java
+++ testgit/src/testgit/hello.java
@@ -3,6 +3,7 @@ package testgit;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("helloWorld��123456");
+		System.out.println("helloWorld£¡123456");
+		System.out.println("git网上提交的！");
 	}
 }
