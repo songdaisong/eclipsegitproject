0c1f3cc7ff6ed8e92a46917c6cb45f982f3b9120
songdaisong <36954321+songdaisong@users.noreply.github.com>
2018-09-04 16:40:27 +0800

    Update hello.java
    111

--- testgit/src/testgit/hello.java
+++ testgit/src/testgit/hello.java
@@ -1,9 +1,10 @@
 package testgit;
 
 public class hello {
-
+	//乱码？
 	public static void main(String[] args) {
 		System.out.println("helloWorld£¡123456");
 		System.out.println("git网上提交的！");
+		System.out.println("hdddd");
 	}
 }
